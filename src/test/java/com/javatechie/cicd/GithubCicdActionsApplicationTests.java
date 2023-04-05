dc4010086a5ffd2e6ae27569615a3d5c2bcd5782
jagadish <jagadesh.ampolu@gmail.com>
2023-04-05 21:57:17 +0530

    Update GithubCicdActionsApplicationTests.java

--- src/test/java/com/javatechie/cicd/GithubCicdActionsApplicationTests.java
+++ src/test/java/com/javatechie/cicd/GithubCicdActionsApplicationTests.java
@@ -1,13 +1,13 @@
 package com.javatechie.cicd;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class GithubCicdActionsApplicationTests {
-
 	@Test
 	void contextLoads() {
+        assertTrue(true);
 	}
-
 }
+
